c6c21bd82e080693b4b1915c73cc118bcad50995
Mina Khosravi <minsmscbook@minas-air.home>
2024-05-20 20:42:54 +0100

    css for navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlineMenu,AiOutlineHome, AiOutlineAppstore, AiOutlineShoppingCart, AiOutlinePhone, AiOutlineSearch } from 'react-icons/ai';
 import './Navbar.css';
@@ -7,38 +7,41 @@ import './Navbar.css';
 const Navbar = () => {
 const [openMenu, setOpenMenu] = useState(false);
 const toggleMenu = ()=> {
-  setOpenMenu(!openMenu);
+  setOpenMenu(openMenu => !openMenu);
+  console.log('openMenu', openMenu)
 };
-
-
 
   return (
     <nav className="navbar">
       <div className="navbar-menu" onClick={toggleMenu}>
         <AiOutlineMenu size={30}/>
       </div>
-      <ul className={`navbar-links ${openMenu ? 'open' : ''}`} >
-        <li>
-          <Link to="/" onClick={toggleMenu}>
-            <AiOutlineHome /> Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/products" onClick={toggleMenu}>
-            <AiOutlineAppstore /> Products
-          </Link>
-        </li>
-        <li>
-          <Link to="/cart" onClick={toggleMenu}>
-            <AiOutlineShoppingCart /> Cart
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" onClick={toggleMenu}>
-            <AiOutlinePhone />Call 
-          </Link>
-        </li>
-      </ul>
+      {openMenu && (
+        <div>
+        <ul className="navbar-links" >
+          <li>
+            <Link to="/" >
+              <AiOutlineHome /> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/products">
+              <AiOutlineAppstore /> Products
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" >
+              <AiOutlineShoppingCart /> Cart
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" >
+              <AiOutlinePhone />Call 
+            </Link>
+          </li>
+        </ul>
+        </div>
+      )}
       <div className="navbar-search">
         <form className="search-bar">
           <input type="text" placeholder="Search..." />
